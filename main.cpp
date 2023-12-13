c4ae9479a6927d7552383e5893ff867f23266d94
OxiaozhaiO <52xiaozhai@gmail.com>
2023-12-13 22:20:35 +0800

    parameter added and updated

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
+#include<string>
+#include<vector>
+#include<cstring>
+
 using namespace std;
-int main()
+
+#include"add.h"
+#include"sub.h"
+int main(int argc, char* argv[])
 {
-	cout<<"hello world"<<endl;
+	char *p = argv[1], *symbol = argv[2];
+	vector<int> a, b;
+	for(int i = 0; *(p+i) != '\0'; i++) a.push_back(*(p+i) - '0');
+	p = argv[3];
+	for(int i = 0; *(p+i) != '\0'; i++) b.push_back(*(p+i) - '0');
+	if(!strcmp(symbol, "+")) add(a, b);
+	else if(!strcmp(symbol, "-")) sub(a, b);
+	return 0;
 }
