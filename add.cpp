a038c6bdba846ebb8adc3d33094923745ca0da29
OxiaozhaiO <52xiaozhai@gmail.com>
2023-12-13 22:21:30 +0800

    Added add and sub functions

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+void add(vector<int> a, vector<int> b)
+{
+	vector<int> c;
+	int temp;
+	for(int i = 0; i < a.size() || b.size(); i++)
+	{
+		 
+	}
+}
---
+++ sub.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void sub(vector<int> a, vector<int> b)
+{
+	cout<<"this is sub"<<endl;
+}
