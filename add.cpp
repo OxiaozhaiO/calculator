884dd12eda72aaa531028a63356afa78e4a2bcd2
OxiaozhaiO <52xiaozhai@gmail.com>
2023-12-18 18:25:51 +0800

    first complete add function

--- add.cpp
+++ add.cpp
@@ -4,9 +4,14 @@ using namespace std;
 void add(vector<int> a, vector<int> b)
 {
 	vector<int> c;
-	int temp;
-	for(int i = 0; i < a.size() || b.size(); i++)
+	int temp = 0;
+	for(int i = 0; i < a.size() || i < b.size() || temp; i++)
 	{
-		 
+		if(i < a.size()) temp += a[i];
+		if(i < b.size()) temp += b[i];
+		c.push_back(temp%10);
+		temp /= 10;
 	}
+	for(int i = c.size() - 1; i >= 0; i--) cout<<c[i];
+	cout<<endl;
 }
