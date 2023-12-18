df2739ea32ecc68d1859aa610bb20bbe76b6ba96
OxiaozhaiO <52xiaozhai@gmail.com>
2023-12-18 18:24:42 +0800

    update

--- main.cpp
+++ main.cpp
@@ -5,15 +5,22 @@
 
 using namespace std;
 
-#include"add.h"
-#include"sub.h"
+#include"include/add.h"
+#include"include/sub.h"
 int main(int argc, char* argv[])
 {
-	char *p = argv[1], *symbol = argv[2];
-	vector<int> a, b;
-	for(int i = 0; *(p+i) != '\0'; i++) a.push_back(*(p+i) - '0');
-	p = argv[3];
-	for(int i = 0; *(p+i) != '\0'; i++) b.push_back(*(p+i) - '0');
+	vector<int> a, b;//容纳两个数字
+	char *strSum1 = argv[1], *strSum2 = argv[3], *symbol = argv[2];// 接收参数 
+	int len1 = -1, len2 = -1;//两个数长度
+
+	//获取数字长度
+	while((strSum1[++len1])!='\0'); while((strSum2[++len2])!='\0');
+
+	//字符串转数字
+	for(int i = len1-1; i >= 0; i--) a.push_back(strSum1[i]-'0');
+	for(int i = len2-1; i >= 0; i--) b.push_back(strSum2[i]-'0');
+
+	//判断符号
 	if(!strcmp(symbol, "+")) add(a, b);
 	else if(!strcmp(symbol, "-")) sub(a, b);
 	return 0;
